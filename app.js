cd9c2f0f5b1fe1ec759e9c341dd7366ac35b3999
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2024-06-04 00:43:57 +0500

    updated files

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ function calculate() {
     let bill = document.getElementById('bill');
     let tip = document.getElementById('tip');
     let total = document.getElementById('total');
-    total.innerText = 'Total : ' + bill.value * (1 + tip.value / 100) ;
+    let percentage = (bill.value * tip.value) / 100;
+    // total.innerText = Number(bill.value) + Number(percentage);
+    let full = Number(bill.value) + Number(percentage);
+    total.innerText = full.toFixed(2);
 }
