cb10b87ae065e63cdd251b23c5c18ab3a0802ca2
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2024-06-04 01:28:18 +0500

    updated files

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ function calculate() {
     let bill = document.getElementById('bill');
     let tip = document.getElementById('tip');
     let total = document.getElementById('total');
-    let percentage = (bill.value * tip.value) / 100;
-    // total.innerText = Number(bill.value) + Number(percentage);
-    let full = Number(bill.value) + Number(percentage);
-    total.innerText = full.toFixed(2);
+    let error = document.getElementById('error');
+    if (bill.value === '' && tip.value === '') {
+        error.innerText = "Please Enter the required fields!";
+    }
+    else if (bill.value === '') {
+        error.innerText = "Please Enter Bill Amount!";
+    }
+    else if (tip.value === '') {
+        error.innerText = "Please Enter Tip Percentage!";
+    }
+    else {
+        let percentage = (bill.value * tip.value) / 100;
+        let full = Number(bill.value) + Number(percentage);
+        total.innerText = full.toFixed(2);
+    }
+    bill.value = '';
+    tip.value = '';
 }
